089fce76470edcf552af4ca444bb17360242d9ce
skpayaya <sashi.kumar.patelskp@gmail.com>
2022-04-12 00:57:45 +0530

    first commit trying out sveltekit

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button>Click</button>
+
+<style>
+	p {
+		color: red;
+		font-size: large;
+	}
+</style>
